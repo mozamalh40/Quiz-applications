77a84744ebe26f3f06f92ad46f585891f802397a
Mozamal Hussain <92964608+mozamalh40@users.noreply.github.com>
2022-08-27 23:51:10 +0500

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      Made ♥ by Mozamal Hussain
+     
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,85 @@
+import { Button, MenuItem, TextField } from "@material-ui/core";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
+import Categories from "../../Data/Categories";
+import "./Home.css";
+
+const Home = ({ name, setName, fetchQuestions }) => {
+  const [category, setCategory] = useState("");
+  const [difficulty, setDifficulty] = useState("");
+  const [error, setError] = useState(false);
+
+  const history = useNavigate();
+
+  const handleSubmit = () => {
+    if (!category || !difficulty || !name) {
+      setError(true);
+      return;
+    } else {
+      setError(false);
+      fetchQuestions(category, difficulty);
+      history("/quiz");
+    }
+  };
+
+  return (
+    <div className="content">
+      <div className="settings">
+        <span style={{ fontSize: 30 }}>Quiz Settings</span>
+        <div className="settings__select">
+          {error && <ErrorMessage>Please Fill all the feilds</ErrorMessage>}
+          <TextField
+            style={{ marginBottom: 25 }}
+            label="Enter Your Name"
+            variant="outlined"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <TextField
+            select
+            label="Select Category"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            variant="outlined"
+            style={{ marginBottom: 30 }}
+          >
+            {Categories.map((cat) => (
+              <MenuItem key={cat.category} value={cat.value}>
+                {cat.category}
+              </MenuItem>
+            ))}
+          </TextField>
+          <TextField
+            select
+            label="Select Difficulty"
+            value={difficulty}
+            onChange={(e) => setDifficulty(e.target.value)}
+            variant="outlined"
+            style={{ marginBottom: 30 }}
+          >
+            <MenuItem key="Easy" value="easy">
+              Easy
+            </MenuItem>
+            <MenuItem key="Medium" value="medium">
+              Medium
+            </MenuItem>
+            <MenuItem key="Hard" value="hard">
+              Hard
+            </MenuItem>
+          </TextField>
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            onClick={handleSubmit}
+          >
+            Start Quiz
+          </Button>
+        </div>
+      </div>
+      <img src="https://images.unsplash.com/photo-1516534775068-ba3e7458af70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" className="banner" alt="quiz app" />
+    </div>
+  );
+};
+
+export default Home;
